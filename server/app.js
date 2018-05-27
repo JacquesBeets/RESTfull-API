e723dc20d50aeb166ae139f456c989b63b903229
jacquesbeets <jacques.beets@gmail.com>
2018-05-27 11:51:05 +0200

    added error handling

--- server/app.js
+++ server/app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const ProductRoutes = require('./api/routes/products')
 const OrderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'))
 
 app.use('/products', ProductRoutes)
 app.use('/orders', OrderRoutes)
 
+app.use((req, res, next) => {
+  const error = new Error('Request Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app
